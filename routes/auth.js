5b8cd719165cae1ca79fc52db090598c42467cfa
dshparko <d.shparko@mail.ru>
2023-01-09 22:33:23 +0300

    feat: add post

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const passport = require("passport");
 
-const CLIENT_URL = "http://localhost:3000/";
+const CLIENT_URL = "https://final-project-client-itra-cvbgp80wg-dshparko.vercel.app/";
 
 
 const {validationResult} = require("express-validator")
