cd547329186e0a9384e0887067c90127da7a3194
dshparko <d.shparko@mail.ru>
2023-01-10 14:09:45 +0300

    fix: fix client url

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(passport.session());
 
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "https://final-project-client-itra.vercel.app/",
         methods: "GET,POST,PUT,DELETE",
         credentials: true,
     })
